93fddf0012869c4c4fc0780c91343498f815fdc8
Sargis Nikolyan <sargisnikolyan@gmail.com>
2020-03-28 23:13:14 +0400

    added ad page

--- src/views/Ads/Ad.vue
+++ src/views/Ads/Ad.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="wrapper">
-    <v-container class="page">
-      <v-layout>
-        <v-flex>
-          <h1>Ad</h1>
+    <v-container class="page" grid-list-lg>
+      <v-layout row>
+        <v-flex xs12 sm10 offset-sm1>
+          <v-card>
+            <v-img src="https://cdn.vuetifyjs.com/images/carousel/bird.jpg" height="300px"></v-img>
+            <v-card-text>
+              <h1 class="text--primary">lorem</h1>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn class="warning" text>Edit</v-btn>
+              <v-btn class="teal">Buy</v-btn>
+            </v-card-actions>
+          </v-card>
         </v-flex>
       </v-layout>
     </v-container>
