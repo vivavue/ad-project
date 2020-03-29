21ce64d718c0e0571848c3cc67bf9d95d48ff542
Sargis Nikolyan <sargisnikolyan@gmail.com>
2020-03-29 18:54:40 +0400

    installed firebase and configured in the main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as fb from 'firebase'
 import './registerServiceWorker'
 import vuetify from './plugins/vuetify'
 import animate from 'animate.css'
@@ -14,5 +15,17 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    fb.initializeApp({
+      apiKey: 'AIzaSyDsyULwA_ctqt_10RQBU5arLZWHlxYolmc',
+      authDomain: 'ad-project-7dfac.firebaseapp.com',
+      databaseURL: 'https://ad-project-7dfac.firebaseio.com',
+      projectId: 'ad-project-7dfac',
+      storageBucket: 'ad-project-7dfac.appspot.com',
+      messagingSenderId: '559492317276',
+      appId: '1:559492317276:web:78380dd8f3cce91ae32699',
+      measurementId: 'G-JPESN0SWS4'
+    })
+  }
 }).$mount('#app')
